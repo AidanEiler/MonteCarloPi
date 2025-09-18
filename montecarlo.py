e328b2a2f78eb204e21023c99172699b4438ce44
Aidan Eiler <eileran@proton.me>
2025-09-17 21:07:26 -0500

    initial commit

---
+++ montecarlo.py
@@ -0,0 +1,18 @@
+'''
+Idea: Generate two points in an XY plane from -1 to 1 and check if they are within a unit circle. If not, they're in a square.
+Then, the ratio of points in the circle to total points approximates PI/4. To approximate PI, multiply the ratio by 4 to get PI = 4 * (points in circle / total points).
+'''
+import random # for generating random points
+
+inside_circle = 0
+PI = 0
+total_points = 0
+for _ in range(10000000): # chosing a large number of points since this isn't a difficult computation
+    x = random.uniform(-1, 1) # generate a random x point from -1 to 1
+    y = random.uniform(-1, 1) # generate a random y point from -1 to 1
+    if x**2 + y**2 <= 1: # equation of a circle is x^2 + y^2 = r^2, here r=1. chatgpt was used to help recall the exponentiation operator in python
+        inside_circle += 1
+    total_points += 1
+
+PI = 4 * (inside_circle / total_points) # calculating PI using the ratio of points in circle to total points
+print("Approximation of PI given 10 million trials is", PI)
